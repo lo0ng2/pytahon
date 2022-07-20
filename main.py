57881215f4fabdc9e6eaf63d8ad5100cb491f484
lo0ng2 <109662891+lo0ng2@users.noreply.github.com>
2022-07-20 19:20:04 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import numpy as np
-a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+a = np.random.randint(1,10, size=(3,3))
+b = np.random.randint(1,10, size=(3,3))
 print(a)
+print(b)
+# cong
+print(a+b)
+# tru
+print(a-b)
+# nhan
+print(a*b)
+# tim nghich dao
+print(np.linalg.inv(a))
+print(np.linalg.inv(b))
+# tim dinh thuc
+print(np.linalg.det(a))
+print(np.linalg.det(b))
+# mu
+print(a**2)
+#rank
+print(np.linalg.matrix_rank(a));
+print(np.linalg.matrix_rank(b));
+# tim ma tran chuyen vi
+print(np.transpose(a))
+print(np.transpose(b))
